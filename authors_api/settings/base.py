23cb4bfa1adb32095e84c7174d01c4c35a7ce9d8
iosif <solomoniosif@gmail.com>
2022-07-24 18:27:55 +0300

    added celery, redis and flower

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -29,6 +29,7 @@ THIRD_PARTY_APPS = [
     'phonenumber_field',
     'drf_yasg',
     'corsheaders',
+    'djcelery_email',
 ]
 
 LOCAL_APPS = [
@@ -149,6 +150,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_URLS_REGEX = r"^/api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND")
+CELERY_TIMEZONE = "Europe/Bucharest"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 LOGGING = {
     'version': 1,
